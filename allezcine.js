f7c7fb67c7b5e27288b600f3af9f47575cd397ee
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-06-18 16:29:16 +0200

    18/6 16h29

---
+++ allezcine.js
@@ -0,0 +1,13 @@
+function clic() {
+  document.getElementById('form1').style.display = "none";
+  // document.getElementById('form2').style.display = "block";
+
+}
+
+// $("#hide").click(function() {
+//   $("form1").hide();
+// });
+//
+// $("#show").click(function() {
+//   $("form2").show();
+// });
