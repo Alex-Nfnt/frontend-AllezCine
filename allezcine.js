19b35f06974cff6bad767495461a2e04d215d860
Antoni Dalle Nogare <antoni.dallenogare@gmail.com>
2018-06-26 12:16:45 +0200

    26/6 12h16

--- allezcine.js
+++ allezcine.js
@@ -20,6 +20,20 @@ function bordure() {
   }
 }
 
+// modal vidéo
+
+$(function() {
+  $(".video").click(function() {
+    var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + ' iframe').attr('src', videoSRCauto);
+    $(theModal + ' button.close').click(function() {
+      $(theModal + ' iframe').attr('src', videoSRC);
+    });
+  });
+});
+
 // Filtre genre film
 
 filterSelection("all")
